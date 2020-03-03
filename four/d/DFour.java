5870ba266740591bd9d214e8b2906f3e0a2c42db
Meek Rose <james.2.meekrose@bt.com>
2020-03-03 11:04:26 +0000

    made some updates

--- four/d/DFour.java
+++ four/d/DFour.java
@@ -1,11 +1,16 @@
 package four.d;
 
 public class DFour {
+	
+	static String myWords = "Hello, am I working - this has been pulled locally! but on the ";
+	static int tries = 100;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Hello, am I working - this has been pulled locally!");
+		
+		
+		System.out.println(myWords + tries + "th time");
 
 	}
 
