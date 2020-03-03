df02f8365df310a719d36a80387394b5494b5808
JMRBT <60648901+JMRBT@users.noreply.github.com>
2020-03-03 07:32:29 +0000

    Update DFour.java

--- four/d/DFour.java
+++ four/d/DFour.java
@@ -5,7 +5,7 @@ public class DFour {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Hello, am I working");
+		System.out.println("Hello, am I working - this has been pulled locally!");
 
 	}
 
